6d1149ad855324010baf419d8550e42155000bb6
Appolo-13 <rayl1x.gta@gmail.com>
2025-02-18 07:40:14 +0100

    Update navbar styles and functionality: change background color to use CSS variable and highlight active menu item

--- src/main/resources/static/js/navbar.js
+++ src/main/resources/static/js/navbar.js
@@ -1,6 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     const menuItems = document.querySelectorAll('.menu');
     const nav = document.querySelector('nav');
+    const currentLocation = window.location.href;
+
+    menuItems.forEach((item) => {
+        if (item.href === currentLocation) {
+            item.classList.add("active");
+        }
+    });
+
     window.addEventListener('scroll', function () {
         if (window.scrollY > 50) {
             nav.style.backgroundColor = 'rgba(255,255,255,0.7)';
